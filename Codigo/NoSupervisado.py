4ae5fc4edd01a40a8f56e0eedbef2afe184d2c7a
desog <201503692@ingenieria.usac.edu.gt>
2021-05-11 08:28:54 -0600

    DataSet Machine Data agregado

--- Codigo/NoSupervisado.py
+++ Codigo/NoSupervisado.py
@@ -1,5 +1,26 @@
 import numpy as np
 from scipy.cluster.vq import kmeans
 import pandas as pd
-path = "https://raw.githubusercontent.com/desog/PropuestaMetodologia/main/Recursos/DataSets/incd.csv"
-dataframe = pd.read_csv(path, encoding = 'latin1')
+from sklearn.neighbors import NearestCentroid
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+path = "https://raw.githubusercontent.com/desog/PropuestaMetodologia/main/Recursos/DataSets/forestfires.csv"
+dataframe = pd.read_csv(path)
+area = dataframe["area"]
+mes = dataframe["month"]
+resultados = dataframe.groupby("month").sum()["area"]
+resultados.sort_index(inplace=True)
+res = resultados.to_frame()
+X = np.array(list(zip(mes,area)))
+
+kmeans = KMeans(n_clusters=3)
+kmeans.fit(X)
+y_kmeans = kmeans.predict(X)
+clf = NearestCentroid()
+clf.fit(X,y_kmeans)
+print("Centroides: ",kmeans.cluster_centers_)
+print("predicción centroide más cercano: " ,clf.predict([[4,4]]))
+plt.xlabel("Aerolineas")
+plt.ylabel("Pasajeros")
+plt.scatter(X[:, 0], X[:, 1], c=y_kmeans, s=30, cmap='rainbow')
+plt.show()
